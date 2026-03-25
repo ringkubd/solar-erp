ed3224efc8a72c1e82363c83b34f68f8ae639b34
Anwar Jahid <ajr.jahid@gmail.com>
2026-03-25 12:51:01 +0600

    d

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.20.231'],
+  allowedDevOrigins: ['192.168.20.231', 'ecopacpowertech.com'],
 };
 
 export default nextConfig;
