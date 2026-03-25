19eb9909b82214c1eb36a80d779b389cd7cfe82e
Anwar Jahid <ajr.jahid@gmail.com>
2026-03-25 18:03:10 +0600

    corse added https://ecopacpowertech.com/

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', ...explode(',', env('ALLOWED_ORIGINS', ''))],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'https://ecopacpowertech.com', ...explode(',', env('ALLOWED_ORIGINS', ''))],
 
     'allowed_origins_patterns' => [],
 
